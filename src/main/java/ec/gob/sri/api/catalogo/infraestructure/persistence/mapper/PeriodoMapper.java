2a9b8cbfe49a68370ba8a9db59474cd80fe86130
Carlos Anchundia <contacto.splendercode@gmail.com>
2025-09-18 10:56:52 -0500

    fix: mappers

--- src/main/java/ec/gob/sri/api/catalogo/infraestructure/persistence/mapper/PeriodoMapper.java
+++ src/main/java/ec/gob/sri/api/catalogo/infraestructure/persistence/mapper/PeriodoMapper.java
@@ -31,7 +31,18 @@ public interface PeriodoMapper {
     })
     Periodo toDomain(PeriodoEntity e);
 
-
+    @Mappings({
+            @Mapping(source = "codigoPeriodo", target = "codigoPeriodo"),
+            @Mapping(source = "anioFiscal", target = "anioFiscal"),
+            @Mapping(source = "periodicidadEntity.codigoPeriodicidad", target = "codigoPeriodicidad"),
+            @Mapping(source = "descripcion", target = "descripcion"),
+            @Mapping(source = "eliminado", target = "eliminado"),
+            @Mapping(source = "estado", target = "estado"),
+            @Mapping(source = "fechaFinal", target = "fechaFinal"),
+            @Mapping(source = "fechaInicial", target = "fechaInicial"),
+            @Mapping(source = "numeroPeriodo", target = "numeroPeriodo"),
+            @Mapping(source = "procesado", target = "procesado")
+    })
     List<Periodo> toDomainList(List<PeriodoEntity> list);
 
     // Domain -> Response DTO
